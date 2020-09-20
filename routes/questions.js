d2660c971232b663db1377a9f83e749b425adf7c
Mark Baker <mrbaker1917@gmail.com>
2020-09-20 11:17:25 -0700

    minor edits to questions.js

--- routes/questions.js
+++ routes/questions.js
@@ -8,12 +8,11 @@ module.exports = (db) => {
       .then(data => {
         let question = data.rows[0].question;
         let templateVars = { question_id: req.params.questionid, question };
-        res.render('../views/question', templateVars);
+        res.render('../views/questions', templateVars);
       });
   });
 
   router.post('/:questionId', (req, res) => {
-    console.log(req.body);
     let values = [req.params.questionId, req.body.answer1, req.body.isCorrecta1, req.params.questionId, req.body.answer2, req.body.isCorrecta2,
     req.params.questionId, req.body.answer3, req.body.isCorrecta3, req.params.questionId, req.body.answer4, req.body.isCorrecta4,];
     let query = `
@@ -22,11 +21,11 @@ module.exports = (db) => {
             ($1, $2, $3), ($4, $5, $6), ($7, $8, $9), ($10, $11, $12)
         RETURNING *;
         `;
-    req.session.question_id = req.params.questionId;
+    req.session.quiz_id = req.params.quiz_id;
     db.query(query, values)
       .then(data => {
         const questionID = data.rows[0].question_id;
-        res.redirect(`/questions/${questionID}`)
+        res.redirect(`/quiz/quizid/questions/${questionID}`)
       })
       .catch(err => {
         res
