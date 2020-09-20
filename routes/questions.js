34a809f333925b38e549d8a39c6574b5aeeda1a7
Mark Baker <mrbaker1917@gmail.com>
2020-09-20 10:34:10 -0700

    implements answers into answers table

--- routes/questions.js
+++ routes/questions.js
@@ -1,28 +1,27 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 module.exports = (db) => {
 
-  router.get('/questions/:questionid', (req, res) => {
+  router.get('/:questionid', (req, res) => {
     db.query(`SELECT question FROM questions WHERE id = $1;`, [req.params.questionid])
-    .then(data => {
-      let question = data.rows[0].question;
-      let templateVars = {question_id: req.params.questionid, question };
-      res.render('../views/question', templateVars);
-    });
+      .then(data => {
+        let question = data.rows[0].question;
+        let templateVars = { question_id: req.params.questionid, question };
+        res.render('../views/question', templateVars);
+      });
   });
 
   router.post('/:questionId', (req, res) => {
     console.log(req.body);
     let values = [req.params.questionId, req.body.answer1, req.body.isCorrecta1, req.params.questionId, req.body.answer2, req.body.isCorrecta2,
-      req.params.questionId, req.body.answer3, req.body.isCorrecta3, req.params.questionId, req.body.answer4, req.body.isCorrecta4, ];
-    console.log(values);
-      let query = `
-     INSERT INTO answers (question_id, answer, isCorrect) VALUES ($1, $2, $3);
-     INSERT INTO answers (question_id, answer, isCorrect) VALUES ($4, $5, $6);
-     INSERT INTO answers (question_id, answer, isCorrect) VALUES ($7, $8, $9);
-     INSERT INTO answers (question_id, answer, isCorrect) VALUES ($10, $11, $12) RETURNING *;
-    `;
+    req.params.questionId, req.body.answer3, req.body.isCorrecta3, req.params.questionId, req.body.answer4, req.body.isCorrecta4,];
+    let query = `
+        INSERT INTO answers (question_id, answer, isCorrect)
+        VALUES
+            ($1, $2, $3), ($4, $5, $6), ($7, $8, $9), ($10, $11, $12)
+        RETURNING *;
+        `;
     req.session.question_id = req.params.questionId;
     db.query(query, values)
       .then(data => {
@@ -34,6 +33,6 @@ module.exports = (db) => {
           .status(500)
           .json({ error: err.message });
       });
-    });
+  });
   return router;
 };
