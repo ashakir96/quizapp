f80cc05d9dd5f08f0f2a794d4665d9c2cb7a706b
Ammar Shakir <shakirammar12@gmail.com>
2020-09-22 21:26:48 +0000

    made adjustments to tables

--- db/schema/06_answer_attempts.sql
+++ db/schema/06_answer_attempts.sql
@@ -3,5 +3,6 @@ DROP TABLE IF EXISTS answer_attempts CASCADE;
 CREATE TABLE answer_attempts (
   id SERIAL PRIMARY KEY NOT NULL,
   answer_id INTEGER REFERENCES answers(id) ON DELETE CASCADE,
-  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  quiz_attempt_id INTEGER REFERENCES quiz_attempts(id) ON DELETE CASCADE
 );
--- db/seeds/05_answer_attempts_insert.sql
+++ db/seeds/05_answer_attempts_insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO answer_attempts (answer_id, user_id) VALUES (2, 1);
-INSERT INTO answer_attempts (answer_id, user_id) VALUES (6, 1);
-INSERT INTO answer_attempts (answer_id, user_id) VALUES (8, 1);
-INSERT INTO answer_attempts (answer_id, user_id) VALUES (12, 1);
+INSERT INTO answer_attempts (answer_id, user_id, quiz_attempt_id) VALUES (2, 1, 1);
+INSERT INTO answer_attempts (answer_id, user_id, quiz_attempt_id) VALUES (6, 1, 1);
+INSERT INTO answer_attempts (answer_id, user_id, quiz_attempt_id) VALUES (8, 1, 1);
+INSERT INTO answer_attempts (answer_id, user_id, quiz_attempt_id) VALUES (12, 1, 1);
