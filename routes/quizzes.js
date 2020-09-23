8e4e88492286e7e7938ac3f8fdbe26bdbba59477
Ammar Shakir <shakirammar12@gmail.com>
2020-09-23 22:26:07 +0000

    changed the data from the a query

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -28,8 +28,11 @@ module.exports = (db) => {
 
   router.get("/:quizid/questions", (req, res) => {
     req.session.quiz_id = req.params.quizid;
-    let templateVar = { quizId: req.params.quizid };
-    res.render('../views/questions', templateVar);
+    db.query(`SELECT id, user_id FROM quizzes WHERE id = $1`, [req.params.quizid])
+    .then(data => {
+      let templateVar = { quizId: req.params.quizid, user: data.rows[0] };
+      res.render('../views/questions', templateVar);
+    })
   })
 
   router.get("/:quizid/questions/:questionid", (req, res) => {
